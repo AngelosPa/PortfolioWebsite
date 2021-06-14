10e94151c6e9019b37ce2c1a125505d2eb882185
angelo.p <mrpalaiologos@gmail.com>
2021-06-14 19:13:54 +0200

    basic layout done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import Aboutme from "./components/Aboutme";
 import Getintouch from "./components/Getintouch";
 import Mywork from "./components/Mywork";
-//import "./scss/Main.scss";
+import "./scss/Main.scss";
 
 function App() {
   return (
     <div>
+      {/* <img src={`${process.env.PUBLIC_URL}/${data[0].img}`} alt="Sushi" /> */}
+
       <Aboutme />
       <Getintouch />
       <Mywork />
