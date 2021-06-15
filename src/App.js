c9f97baeecbe88359a500e5b06d92d9729bf5cf9
angelo.p <mrpalaiologos@gmail.com>
2021-06-15 17:01:40 +0200

    first back up about to finish aboutme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//Browserrouter aka Router to wrap everything
+//switch to looks through its children <Route>s and renders the first one that matches the current URL. ad lets go
+
 import Aboutme from "./components/Aboutme";
 import Getintouch from "./components/Getintouch";
 import Mywork from "./components/Mywork";
@@ -6,14 +10,32 @@ import "./scss/Main.scss";
 
 function App() {
   return (
-    <div>
-      {/* <img src={`${process.env.PUBLIC_URL}/${data[0].img}`} alt="Sushi" /> */}
-
-      <Aboutme />
-      <Getintouch />
-      <Mywork />
-    </div>
+    <Router>
+      <div className="body-container">
+        {/* you will need it for the pics */}
+        {/* <img src={`${process.env.PUBLIC_URL}/${data[0].img}`} alt="Sushi" /> */}
+        <h1>Welcome to the official website of Angelo</h1>{" "}
+        <Switch>
+          <Route path="/PortfolioWebsite" exact>
+            <Aboutme />
+          </Route>
+          <Route path="/mywork" exact>
+            <Mywork />
+          </Route>
+          <Route path="/getintouch" exact>
+            <Getintouch />
+          </Route>
+          <Route path={() => "/main" || "/admin" || "/any-other-word"}>
+            wrong way..
+          </Route>
+        </Switch>
+        <footer>
+          <h5>Made with ❤️ By Angelo&nbsp;</h5>
+          <h6>All copyrights reserved ® {new Date().getFullYear()}</h6>
+        </footer>
+      </div>
+    </Router>
   );
 }
-
+//for the vid https://nextjs.org/conf/tickets/jun21/AngelosPa
 export default App;
--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,5 +1,64 @@
+import AOS from "aos";
+import Techno from "./Techno";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import React, { useState, useEffect } from "react";
+// ..
+AOS.init();
+
 function Aboutme() {
-  return <div>zxdfsthgn</div>;
+  //usetate to understand how much the user scrolled
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
+  const [scroller, setScroller] = useState(0);
+  //window.pageYOffset returns the number of pixels the document is currently scrolled along the vertical(y) axis
+  const handleScroll = () => setScroller(window.pageYOffset);
+  //useeffect to initialize it as soon as the component mounts!!
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    //we need to stop it when the component gets unmounted otherwise we left this listener active and slow down our things
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  console.log(scroller);
+  //we initialise Aos by using our useEffect hook
+  useEffect(() => {
+    AOS.init({});
+  }, []);
+  return (
+    <div
+      className="about-me"
+      // style={{ opacity: `${scroller * 2}%` }}
+      style={{ opacity: `${scroller * 2}%` }}
+    >
+      <div>
+        <p>
+          Angelo is an aspiring newcomer developer currently based in Leipzig,
+          Germany. He entered in 2019 into the world of web engineering as a
+          student of the DCI (Digital Career Institute). After some years of
+          traveling across Europe and collecting experiences he discovered his
+          passion for programming, creating UI effects, animations and
+          developing unique, dynamic user experiences as well digital drawing
+          and 3d effects.
+        </p>
+        <h2> Technologies :</h2>{" "}
+      </div>
+      <div style={{ transform: `translateY(-${scroller * 0.5}px)` }}>
+        <Techno />
+      </div>
+    </div>
+  );
 }
 
 export default Aboutme;
+
+{
+  /* <div
+data-aos="fade-up"
+data-aos-offset="200"
+data-aos-delay="50"
+data-aos-duration="1000"
+data-aos-easing="ease-in-out"
+data-aos-mirror="true"
+data-aos-once="false"
+data-aos-anchor-placement="top-center"
+>
+</div> */
+}
